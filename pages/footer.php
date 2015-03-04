209805819cbef78b1f9d7f960fe5d25dfb7008c5
Rick van Osta <osta1995@hotmail.com>
2015-03-04 15:49:31 +0100

    bootstrap

--- pages/footer.php
+++ pages/footer.php
@@ -1,1 +1,10 @@
-<?php
+<footer>
+    <div class="container-fluid footer">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <p>&copy; 2015 Snelfietsbel.nl<br/>
+               </p>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ pages/shop.php
@@ -0,0 +1,15 @@
+<?php include_once 'head.php'; ?>
+<body>
+<?php include_once 'header.php'; ?>
+
+Shop
+
+
+<?php include_once 'footer.php'; ?>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="../js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+<script src="../js/vendor/bootstrap.min.js"></script>
+<script src="../js/main.js"></script>
+</body>
+</html>
