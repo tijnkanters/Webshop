980d616b9009767dc0a794c5bad0d168c0552daf
Tijn Kanters <trkanter@avans.nl>
2015-03-04 16:07:22 +0100

    Insex

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
-<html>
-
+<?php include_once 'head.php'; ?>
 <body>
+<?php include_once 'header.php'; ?>
 
+Index
+
+
+<?php include_once 'footer.php'; ?>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="../js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+<script src="../js/vendor/bootstrap.min.js"></script>
+<script src="../js/main.js"></script>
 </body>
 </html>
-
