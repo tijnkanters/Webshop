5a2f8a63d2077395d49c03fcfb6ad4e93d130318
Rick van Osta <osta1995@hotmail.com>
2015-03-05 12:41:46 +0100

    product page

---
+++ pages/product.php
@@ -0,0 +1,33 @@
+<?php include_once 'head.php'; ?>
+<body>
+<?php include_once 'header.php'; ?>
+
+<div class="block">
+    <div class="container">
+    	<div class="row">
+    		<div class="col-md-4 col-md-offset-2">
+                <h1>Fietsbel</h1>
+                <h3>Herenmodel</h3>
+                <img src="/Webshop/img/zilverfietsbel.jpg">
+                <p>
+                Een stijlvolle klassieker met nostalgische invloeden. 
+                Deze zilvere fietsbel is helemaal in de mode op iedere nieuwe herenfiets.
+                </p>
+                <h2>&euro;7,95
+                
+                <button name="order" type="submit" class="btn btn-info pull-right ">In winkelwagen</button>
+                </h2>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php include_once 'footer.php'; ?>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="../js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+<script src="../js/vendor/bootstrap.min.js"></script>
+<script src="../js/main.js"></script>
+</body>
+</html>
