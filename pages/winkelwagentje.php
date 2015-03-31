d434d741ca7ec3629970c7a0869a1d33df93064c
Tijn Kanters <trkanter@avans.nl>
2015-03-31 22:12:23 +0200

    Winkelwagentje is wel niet zo fokkin geil nu he

--- pages/winkelwagentje.php
+++ pages/winkelwagentje.php
@@ -19,24 +19,52 @@
             <?php
                 if(count($_SESSION['cart']) == 0){
                     echo "<p>Er zit nog niks in je winkelwagentje.</p>";
+                    exit();
                 }
             ?>
             <p>
+            <form action="winkelwagentje.php" method="post">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Afbeelding</th>
+                            <th>Naam</th>
+                            <th>Prijs</th>
+                            <th>Acties</th>
+                        </tr>
+                    </thead>
+
                 <?php
                 $cart = array();
                 $cart = $_SESSION['cart'];
-                
+                $totalprice = 0;
+                $productcounter = 0;
+
+                if($_SERVER['REQUEST_METHOD'] == 'POST') {
+                    unset($cart[$_POST['product']]);
+                    $filteredCart = array_values( array_filter($cart) );
+                    $cart = $filteredCart;
+                    $_SESSION['cart'] = $cart;
+                }
+
                 foreach ($cart as $c){
-                echo '<div style="border:2px solid lightgray; height:104px;">';
+                    $totalprice = $totalprice + $c->price;
                 
-                echo '<img src="/Webshop/img/' . $c->img . '" style="float: left; width: 100px; height: 100px; border:3px solid gray;">';
-                echo '<br>';
-                echo $c->name;
-                echo $c->price;
-                echo '</div><br>';
+                echo '<tr><td><img src="/Webshop/img/' . $c->img . '" style="float: left; width: 100px; height: 100px; border:3px solid gray;"></td>';
+                echo "<td>".$c->name."</td>";
+                    echo "<td>€ ".$c->price."</td>";
+                    echo "<td><button class='btn btn-default' type='submit' name='product' value=".$productcounter.">Verwijderen</button></td>";
+
+                    $productcounter++;
                 }
                 session_write_close();
+
                  ?>
+                <tr>
+                    <td></td><td style="text-align: right; font-weight: bold;">Totaalprijs:</td><td style="font-weight: bold;">€ <?php echo number_format((float)$totalprice, 2, '.', ''); ?></td><td><button class='btn btn-primary'>Afrekenen</button></td>
+                </tr>
+                </table>
+                </form>
             </p>
         </div>
     </div>
